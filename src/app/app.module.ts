0476ed6debedcabb770520f0bac88b61dc89a3b9
Melcn <melaniearinosy@outlook.com>
2023-05-24 11:57:29 +0200

    update/fix: api connection + geolocation + clean code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IndexComponent } from './core/not-found/pages/index/index.component';
-import { WeatherComponent } from './weather/weather.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
-  declarations: [AppComponent, IndexComponent, WeatherComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
+
 export class AppModule {}
